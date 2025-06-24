7acc25d82278448012816680e4386dd43dc3afc2
Varun Verma <varun.verma@cloudwerx.tech>
2025-06-24 18:04:45 +0530

    Adding tf files

--- terraform/frontend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Substitute BACKEND_URL in index.html
-envsubst < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.tmp \
-    && mv /usr/share/nginx/html/index.tmp /usr/share/nginx/html/index.html
-
-# Start nginx
-nginx -g 'daemon off;'
